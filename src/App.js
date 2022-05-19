0257b30e4878f3405692c6a76c0f0e91db4dd297
subbumv1 <subbumv@gmail.com>
2022-05-19 13:22:02 +0530

    change2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Fast
+          Learn React Fast - More fast
         </a>
       </header>
     </div>
