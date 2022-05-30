fba16adc5e9691a3f736a08902cf579e6a4d5f94
subbumv1 <105791119+subbumv1@users.noreply.github.com>
2022-05-30 13:42:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Fast - More fast 2, still fast
+          Learn React Fast - More fast 2, still fast, super fast!
         </a>
       </header>
     </div>
